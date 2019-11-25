0daea4c0c9b07ff7eabdc41ae1dcde5a9f4bcda7
hazzzi <hanee_l@naver.com>
2019-11-25 12:31:50 +0900

    map Recap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
-function Food({ favourite }) {
-  return <h1>I like {favourite}</h1>
+function Food({ name, picture }) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} width='200px'></img>
+  </div>
 }
+
+const foodILike = [
+  {
+    name: "kimchi",
+    image: "http://placeimg.com/640/480/animals",
+  },
+  {
+    name: "mandoo",
+    image: "http://placeimg.com/640/480/animals",
+  },
+  {
+    name: "ramen",
+    image: "http://placeimg.com/640/480/animals",
+  }
+];
 
 function App() {
   return (
     <div>
       <h1>Hello!!</h1>
-      <Food favourite="kimchi" />
-      <Food favourite="ramen" />
-      <Food favourite="삼겹살" />
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
