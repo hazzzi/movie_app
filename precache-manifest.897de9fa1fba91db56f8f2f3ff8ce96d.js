bcdd4aa7bdfaabcfa8198eb5c833a03d3f8599d9
hazzzi <hanee_l@naver.com>
2019-12-10 15:33:32 +0900

    Updates

---
+++ precache-manifest.897de9fa1fba91db56f8f2f3ff8ce96d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2950adc075cb9ddfa98d59a5f877c390",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "b2bca85830677461f6b1",
+    "url": "/movie_app/static/css/main.cdb5afce.chunk.css"
+  },
+  {
+    "revision": "6f0599dff9eecd2a0e73",
+    "url": "/movie_app/static/js/2.43e6c5f9.chunk.js"
+  },
+  {
+    "revision": "b2bca85830677461f6b1",
+    "url": "/movie_app/static/js/main.34191081.chunk.js"
+  },
+  {
+    "revision": "7259bafd2195d7f70cbd",
+    "url": "/movie_app/static/js/runtime-main.c454ae6d.js"
+  }
+]);
